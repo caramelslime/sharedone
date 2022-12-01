453f00f3ec2801709c71f7633c58f78aca07b347
user <user@DESKTOP-OOMH3HQ>
2022-12-01 11:13:16 +0900

    sql 추가

--- sharedone/src/main/resources/sharedoneDDL.sql
+++ sharedone/src/main/resources/sharedoneDDL.sql
@@ -1,20 +1,42 @@
-drop table employee;
+drop table m_employee cascade constraint;
+drop table m_product cascade constraint;
+drop table M_BUYER cascade constraint;
 --직원 테이블
-CREATE TABLE employee (
-	EMPCD	varchar(20)	NOT NULL,
+CREATE TABLE m_employee (
+	EMPCD	varchar(20)	NOT NULL PRIMARY KEY,
 	PW	varchar(40)	NOT NULL,
 	PHONE_NO	VARCHAR(20)	NOT NULL,
     name varchar(20) NOT NULL,
 	EMAIL	VARCHAR(50)	NOT NULL,
 	HIREDATE	date	NOT NULL,
-	JOB	VARCHAR(10)	NOT NULL,
-	rank	VARCHAR(10)	NOT NULL,
-	authority	char(1) DEFAULT 'n'   NULL
+    DEPT VARCHAR(20) NOT NULL,
+	JOB	VARCHAR(10)	NOT NULL
 );
 -- 판매부번 테이블
-CREATE TABLE product (
+CREATE TABLE m_product (
 	productcd VARCHAR2(20) PRIMARY KEY,
 	productnm VARCHAR2(50),
 	unit VARCHAR2(10),
 	productgroup VARCHAR2(20)
+);
+--거래처 테이블
+CREATE TABLE M_BUYER (
+	BUYERCD	VARCHAR2(20) PRIMARY KEY,		--거래처코드
+	BUYERNM	VARCHAR2(50) NOT NULL,			--거래처명
+	BRNO CHAR(10) NOT NULL,					--사업자등록번호
+	RPRSV_NM VARCHAR2(40) NOT NULL,			--대표자
+	BUSINESS_STATUS	VARCHAR2(200) NOT NULL,	--업태
+	EVENT VARCHAR2(200) NOT	NULL,			--종목
+	RECEIVEUSERID VARCHAR2(40) NOT NULL,	--담당자
+	STATUS VARCHAR2(20) NOT NULL,			--거래처상태
+	NATIONCD CHAR(2) NOT NULL,				--국가코드
+	POSTCODE CHAR(5) NOT NULL,				--우편번호
+	ADDRESS	VARCHAR2(50) NOT NULL,			--소재지
+	ADDRESS_DETAIL VARCHAR2(50) NOT NULL,	--소재지상세
+	TEL	CHAR(11) NOT NULL,					--전화번호
+	EMAIL VARCHAR2(320)	NULL,				--이메일
+	REMARK VARCHAR2(1000) NULL,				--참고사항
+	ADDDATE	DATE NOT NULL,					--등록일시
+	ADDUSER	VARCHAR2(255) NOT NULL,			--작성자
+	DELYN CHAR DEFAULT 'n' NOT NULL			--삭제여부
 );
---
+++ sharedone/src/main/resources/sharedoneDML-insert.sql
@@ -0,0 +1,10 @@
+--회원 데이터
+insert into employee values('E0001','1234','010-8838-0247','김현서','ksh98520@naver.com','1998-07-13','영업1','staff');
+insert into employee values('E0002','1234','010-8838-0245','엄준식','ksh624@naver.com','1998-07-05','영업1','staff');
+insert into employee values('E0003','1234','010-2634-0247','하인호','ksh5325@naver.com','1995-03-24','영업1','staff');
+insert into employee values('E0004','1234','010-7457-0247','최인석','ksh93250@naver.com','1990-06-22','영업1','staff');
+insert into employee values('E0005','1234','010-3067-4088','강병준','kbj010@naver.com','2002-03-29','영업1','manager');
+--제품 데이터
+--거래처 데이터
+--판매가격 데이터
+--오더 데이터
